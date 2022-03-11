d8c2a30f3fe996f19431c81965c9b207981a5b6e
Yusuke Ohi <yohi.consadole12@gmail.com>
2022-03-11 16:07:19 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,30 +11,44 @@ DOCKER_COMPOSE_SERVICE_NAME=${INPUT_DOCKER_COMPOSE_SERVICE_NAME:-"app"}
 echo Host Python Version Check...
 HOST_PYTHON_VERSION=$(python3 --version | sed -E "s/\s|\.[0-9]+$//g")
 
+if [ -d docker-compose.override.yml ]; then
+    mv docker-compose.override.yml docker-compose.override.yml.bk
+fi
+
+cat <<EOF > docker-compose.override.yml
+volumes:
+    ${DOCKER_COMPOSE_SERVICE_NAME}_site-packages-data:
+EOF
+
 echo Docker Python Version Check...
 DOCKER_PYTHON_VERSION=$(docker-compose run --rm ${DOCKER_COMPOSE_SERVICE_NAME} python3 --version | sed -E "s/\s|\.[0-9]+\s*$//g")
+
+cat <<EOF >> docker-compose.override.yml
+services:
+    ${DOCKER_COMPOSE_SERVICE_NAME}:
+      volumes:
+        - ${DOCKER_COMPOSE_SERVICE_NAME}_site-packages-data:/usr/local/lib/${DOCKER_PYTHON_VERSION,,}/site-packages
+EOF
+
+VOLUME_PATH=$(docker inspect --format='{{.Mounts}}' $(docker-compose ps -q central) | tr " " "\n" | grep volume | grep site-packages-data)
 
 if [ -d ${APPLICATION_ROOT}/.venv ]; then
     rm -rf ${APPLICATION_ROOT}/.venv
 fi
 
 python3 -m venv ${APPLICATION_ROOT}/.venv
+
+cd ${APPLICATION_ROOT}/.venv/lib/${HOST_PYTHON_VERSION,,}
+rm -rf site-packages
+ln -s ${VOLUME_PATH} site-packages
+cd -
+
 cat <<EOF > ${APPLICATION_ROOT}/.envrc
 source .venv/bin/activate
 EOF
 direnv allow ${APPLICATION_ROOT}
 wait
 
-if [ -d docker-compose.override.yml ]; then
-    mv docker-compose.override.yml docker-compose.override.yml.bk
-fi
-
-cat <<EOF > docker-compose.override.yml
-services:
-    ${DOCKER_COMPOSE_SERVICE_NAME}:
-      volumes:
-        - ./${APPLICATION_ROOT}/.venv/lib/${HOST_PYTHON_VERSION,,}/site-packages:/usr/local/lib/${DOCKER_PYTHON_VERSION,,}/site-packages
-EOF
 
 echo Install Pip Package...
 docker-compose build
