50ea6c7b630a9323a93c2a48cbfa0589b5a9d862
Yusuke Ohi <y.ohi@diamondhead.tech>
2022-05-05 00:32:25 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+ESC=$(printf '\033')
+
 echo Application Root? [default: django/app]:
 read INPUT_APPLICATION_ROOT
 
@@ -10,7 +13,7 @@ DOCKER_COMPOSE_SERVICE_NAME=${INPUT_DOCKER_COMPOSE_SERVICE_NAME:-"app"}
 
 echo Host Python Version Check...
 HOST_PYTHON_VERSION=$(python3 --version | sed -E "s/\s|\.[0-9]+$//g")
-echo ${HOST_PYTHON_VERSION}
+printf "${ESC}[31m%s${ESC}[m\n" ${HOST_PYTHON_VERSION}
 
 if [ -d docker-compose.override.yml ]; then
     mv docker-compose.override.yml docker-compose.override.yml.bk
@@ -22,9 +25,8 @@ volumes:
 EOF
 
 echo Docker Python Version Check...
-docker-compose build ${DOCKER_COMPOSE_SERVICE_NAME}
 DOCKER_PYTHON_VERSION=$(docker-compose run ${DOCKER_COMPOSE_SERVICE_NAME} python3 --version | sed -E "s/\s|\.[0-9]+\s*$//g")
-echo ${DOCKER_PYTHON_VERSION}
+printf "${ESC}[31m%s${ESC}[m\n" ${DOCKER_PYTHON_VERSION}
 
 cat <<EOF >> docker-compose.override.yml
 services:
@@ -33,7 +35,10 @@ services:
         - ${DOCKER_COMPOSE_SERVICE_NAME}_site-packages-data:/usr/local/lib/${DOCKER_PYTHON_VERSION,,}/site-packages
 EOF
 
+echo Volume Path...
+docker-compose create ${DOCKER_COMPOSE_SERVICE_NAME}
 VOLUME_PATH=$(docker inspect --format='{{.Mounts}}' $(docker-compose ps -q ${DOCKER_COMPOSE_SERVICE_NAME}) | tr " " "\n" | grep volume | grep site-packages-data)
+printf "${ESC}[31m%s${ESC}[m\n" ${VOLUME_PATH}
 
 if [ -d ${APPLICATION_ROOT}/.venv ]; then
     rm -rf ${APPLICATION_ROOT}/.venv
@@ -44,14 +49,13 @@ python3 -m venv ${APPLICATION_ROOT}/.venv
 cd ${APPLICATION_ROOT}/.venv/lib/${HOST_PYTHON_VERSION,,}
 rm -rf site-packages
 ln -s ${VOLUME_PATH} site-packages
-cd -
+cd - 1> /dev/null
 
 cat <<EOF > ${APPLICATION_ROOT}/.envrc
 source .venv/bin/activate
 EOF
 direnv allow ${APPLICATION_ROOT}
 wait
-
 
 echo Install PIP Package...
 docker-compose run --rm ${DOCKER_COMPOSE_SERVICE_NAME} pip install -r /tmp/requirements-dev.txt
